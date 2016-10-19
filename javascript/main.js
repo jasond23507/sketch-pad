54ddfdc8c92bc5b22b284c7e33f3cdad16e68093
Jason Delgado <jasond2350@gmail.com>
2016-10-19 10:24:05 -0400

    The program sketches blue on white squares

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	displayGrid(16);
+	$('.box').mouseenter(function(){
+		$(this).css("background", "blue");
+	})
+
+});
+
+function displayGrid(n){
+	var size = 960;
+	var boxSize = (size - 4*n) / n;
+	var $wrapper = $('.wrapper').html('');
+	for(var i = 0; i < n; i++){
+		for(var j = 0; j < n; j++){
+			$wrapper.append($('<div></div>').addClass("box").width(boxSize).height(boxSize) );
+		}
+		$wrapper.append($('<div></div>').css("clear", "both"));
+	}
+}
